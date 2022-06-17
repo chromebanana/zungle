8bff99dd8251047bbaffc31aa9419a8e97ceb810
William Cragg <wstc@riseup.net>
2022-06-17 14:09:07 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,55 @@
+
+from api import API
+
+
+app = API()
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from the HOME page"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from the ABOUT page"
+
+@app.route("/hello/{name}")
+def greeting(request, response, name):
+    response.text = f"Hello, {name}"
+
+@app.route("/sum/{num_1:d}/{num_2:d}")
+def sum(request, response, num_1, num_2):
+    total = int(num_1) + int(num_2)
+    response.text = f"{num_1} + {num_2} = {total}"
+
+@app.route("/book")
+class BooksResource:
+    def get(self, req, resp):
+        resp.text = "Books Page"
+    
+    def post(self, req, resp):
+        resp.text = "Endpoint to create a book"
+    
+    def delete(self, req, resp):
+        resp.text = "deleting book"
+
+
+def handler(req, resp):
+    resp.text = "sample"
+
+app.add_route("/sample", handler)
+
+@app.route("/template")
+def template_handler(req, resp):
+    resp.body = app.template(
+        "index.html", context={"name": "Bumbo", "title": "Best Framework"}).encode()
+
+
+def custom_exception_handler(request, response, exception_cls):
+    response.text = str(exception_cls)
+
+app.add_exception_handler(custom_exception_handler)
+
+
+@app.route("/exception")
+def exception_throwing_handler(request, response):
+    raise AssertionError("This handler should not be used.")
